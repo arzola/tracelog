3b9c327b8f32800bd81db98fc7feb5f8296333a0
Oscar Arzola <oscararzola@gmail.com>
2016-10-11 17:25:58 -0500

    Fixed backend user facade

--- icodemx/tracelog/classes/LastEdited.php
+++ icodemx/tracelog/classes/LastEdited.php
@@ -10,22 +10,25 @@ trait LastEdited
     {
         static::extend(function ($model) {
             $model->morphMany['logs'] = ['Icodemx\Tracelog\Models\Log', 'name' => 'traceable'];
-            $model->bindEvent('model.afterSave', function () use ($model) {
-                $log = new Log();
-                $log->user = BackendAuth::getUser();
-                $log->userfallback = BackendAuth::getUser()->login . ': ' . BackendAuth::getUser()->first_name . ' ' . BackendAuth::getUser()->last_name;
-                $log->msg = (isset($model->message)) ? $model->message : 'Updated';
-                $log->reference = (isset($model->referenceLog)) ? $model->{$model->referenceLog} : '';
-                $model->logs()->save($log);
-            }, 500);
-            $model->bindEvent('model.afterDelete', function () use ($model) {
-                $log = new Log();
-                $log->user = BackendAuth::getUser();
-                $log->userfallback = BackendAuth::getUser()->login . ': ' . BackendAuth::getUser()->first_name . ' ' . BackendAuth::getUser()->last_name;
-                $log->msg = (isset($model->message)) ? $model->message : 'Deleted';
-                $log->reference = (isset($model->referenceLog)) ? $model->{$model->referenceLog} : '';
-                $model->logs()->save($log);
-            }, 500);
+            if (BackendAuth::getUser() && \App::runningInBackend()) {
+                $user = BackendAuth::getUser()->login . ': ' . BackendAuth::getUser()->first_name . ' ' . BackendAuth::getUser()->last_name;
+                $model->bindEvent('model.afterSave', function () use ($model, $user) {
+                    $log = new Log();
+                    $log->user = BackendAuth::getUser();
+                    $log->userfallback = $user;
+                    $log->msg = (isset($model->message)) ? $model->message : 'Updated';
+                    $log->reference = (isset($model->referenceLog)) ? $model->{$model->referenceLog} : '';
+                    $model->logs()->save($log);
+                }, 500);
+                $model->bindEvent('model.afterDelete', function () use ($model, $user) {
+                    $log = new Log();
+                    $log->user = BackendAuth::getUser();
+                    $log->userfallback = $user;
+                    $log->msg = (isset($model->message)) ? $model->message : 'Deleted';
+                    $log->reference = (isset($model->referenceLog)) ? $model->{$model->referenceLog} : '';
+                    $model->logs()->save($log);
+                }, 500);
+            }
         });
     }
 
